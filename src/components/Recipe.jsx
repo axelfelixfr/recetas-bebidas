3cff62a19f91c6d46e2276483533c92b7f228c2e
Axel <axelfelix123@hotmail.com>
2021-08-27 23:42:53 -0500

    Modal de material-ui agregado

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,12 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ModalContext } from '../context/ModalContext';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  }
+}));
 
 export const Recipe = ({ recipe }) => {
   // Extraer valores del context
   const { setIdRecipe } = useContext(ModalContext);
 
+  // Configuración del modal de material-ui
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCloseModal = () => {
+    handleClose();
+    setIdRecipe(null);
+  };
+
   const showModalRecipe = () => {
     setIdRecipe(recipe.idDrink);
+    handleOpen();
   };
 
   return (
@@ -28,6 +71,12 @@ export const Recipe = ({ recipe }) => {
           >
             Ver receta
           </button>
+
+          <Modal open={open} onClose={handleCloseModal}>
+            <div style={modalStyle} className={classes.paper}>
+              <h1>Desde modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -1,9 +1,28 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const ModalContext = createContext();
 
 const ModalProvider = props => {
+  // State del provider
   const [idRecipe, setIdRecipe] = useState(null);
+  const [fullRecipe, setFullRecipe] = useState({});
+
+  // Una vez que haya una receta, llamar la API
+  useEffect(() => {
+    const getRecipe = async () => {
+      if (!idRecipe) return null;
+
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+
+      const {
+        data: { drinks }
+      } = await axios.get(url);
+      setFullRecipe(drinks[0]);
+    };
+
+    getRecipe();
+  }, [idRecipe]);
 
   return (
     <ModalContext.Provider value={{ setIdRecipe }}>
